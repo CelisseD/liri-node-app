b385ee1b8a83cefb0abaaf366e70c74b5342f3d8
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-06-24 00:50:31 -0400

    added variables to liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,10 @@
-js
+// Hides my personal keys
 require("dotenv").config();
+
+// Variables for required packages
+var keys = require("./keys");
+var moment = require("moment");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var fs = require("fs");
 
