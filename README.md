7db78ab03451558d35d8c1a6ee9f7c2fb7852fca
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-06-26 20:19:47 -0400

    finished with all JS functions, added images to readme

--- README.md
+++ README.md
@@ -5,29 +5,41 @@ LIRI node application. This application was built as part of the UCF Coding Boot
 LIRI is a command line virtual assistant that listens for a a text based user command, and returns information from the Bands in Town API, Online Movie Database (OMDB) API, and Spotify API.
 
 # Cloning
-In order to clone this app, you will need to provide your own .emv file that holds keys for the Spotify and Bands in Town API's.
+In order to clone this repo, you will need to provide your own .emv file that holds keys for the Spotify API.
 
 # User Commands
-// <b>concert-this [artist]</b> - returns upcoming concert dates and venues from Bands in Town API.
+### // <b>concert-this [artist]</b>
+returns upcoming concert dates and venues from Bands in Town API.
 
-// <b>spotify-this-song [song name]</b> - returns Song Name, Artist Name, Album Title, and Preview Link from Spotify API.
+### // <b>spotify-this-song [song name]</b>
+returns Song Name, Artist Name, Album Title, and Preview Link from Spotify API.<br>
+If no [song name] is entered, will return default search for "The Sign" by Ace of Base.
 
-// <b>movie-this [movie name]</b> - returns Movie Name, Release Year, IMDB and Rotten Tomatoes ratings, Country of Production, and Language, from OMDB API.
+### // <b>movie-this [movie name]</b>
+returns Movie Name, Release Year, IMDB and Rotten Tomatoes ratings, Country of Production, and Language, from OMDB API.<br>
+If no [movie name] is entered, will return default search for "Mr. Nobody."
 
-// <b>do-what-it-says</b> - returns search result from random.txt file.
+### // <b>do-what-it-says</b>
+returns search result from random.txt file. Run this command to get info on the best song ever written. 🤪
 
 # Screenshots
-Screenshots will go here
+<img src="images/spotify.png" width=900><br><br>
+<img src="images/OMDB.png"  width=900><br><br>
+<img src="images/BIT.png"  width=500><br><br>
+<img src="images/dowhatitsays.png"  width=900>
 
 # Technologies Used
-JavaScript<br>
-Spotify API<br>
-Bands in Town API<br>
-OMDB API<br>
-node.js<br><br>
+<ul>
+    <li>JavaScript</li>
+    <li>Spotify API</li>
+    <li>Bands in Town API</li>
+    <li>OMDB API</li>
+    <li>node.js</li>
+</ul>
 
-Node Modules:<br>
-Chalk<br>
-Axios<br>
-Moment
+<ul>Node Modules:
+    <li>Chalk</li>
+    <li>Axios</li>
+    <li>Moment</li>
+</ul>
 
--- liri.js
+++ liri.js
@@ -25,48 +25,49 @@ var userInput = process.argv.slice(3).join(" ");
 // SPOTIFY FUNCTION
 function spotifyThis() {
 
-    if (userInput === undefined || null) {
-        userInput = "Ace of Base";
+    if (userInput === "" && process.argv[3] === undefined) {
+        userInput = "The Sign Ace of Base";
     }
-    
-    else {
     
     spotify
         .search({ type: "track", query: userInput, limit: 1})
         .then(function(response) {
-            console.log(chalk.red("\n============= ") + chalk.green("S P O T I F Y  T H I S  S O N G ") + chalk.red("============= \n"));
+            console.log(chalk.green.bgRed("\n============= ") + chalk.green.bold("S P O T I F Y  T H I S  S O N G ") + chalk.green.bgRed("============= \n"));
             console.log(chalk.bold("Song Name: ") + JSON.stringify(response.tracks.items[0].name, null, 2));
             console.log(chalk.bold("Artist Name: ") + JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
             console.log(chalk.bold("Album Name: ") + JSON.stringify(response.tracks.items[0].album.name, null, 2));
             console.log(chalk.bold("Preview Link: ") + chalk.blue(JSON.stringify(response.tracks.items[0].preview_url, null, 2)));
-            console.log(chalk.red("\n============= ") + chalk.green("S P O T I F Y  T H I S  S O N G ") + chalk.red("============= \n"));
+            console.log(chalk.green.bgRed("\n============= ") + chalk.green.bold("S P O T I F Y  T H I S  S O N G ") + chalk.green.bgRed("============= \n"));
         })
         .catch(function(err) {
             console.log(err);
         });
-}};
+};
 
 // DO WHAT IT SAYS FUNCTION
 
 function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function(error,response) {
+    fs.readFile("random.txt", "utf8", function(error,data) {
         if (error) {
             console.log(error);
-        } else {
-        var response = response.split(",");
-        spotifyThis(response);
-        }
+        } 
+        var dataArr = data.split(",");
+        whatToDo = dataArr[0];
+        userInput = dataArr[1];
+        // console.log(dataArr[1])
+        spotifyThis(dataArr[1]);
+        
     });
+
+    
 };
 
 // OMDB FUNCTION
 function movieThis() {
 
-    if (userInput === undefined || null) {
+    if (userInput === "") {
         userInput = "Mr. Nobody";
     }
-    
-    else {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
@@ -76,14 +77,17 @@ function movieThis() {
     axios.get(queryUrl)
     .then(function(response) {
         // console.log(JSON.stringify(response, null, 2));
-        console.log(chalk.green("\n============= ") + chalk.magentaBright("S P O T I F Y  T H I S  S O N G ") + chalk.green("============= \n"));
-        console.log("Movie Title: " + JSON.stringify(response.data.Title, null, 2));
-        console.log("Release Year: " + JSON.stringify(response.data.Year, null, 2));
-        console.log("IMDB Rating: " + JSON.stringify(response.data.Ratings[0].Value, null, 2));
-        console.log("Rotten Tomatoes Rating: " + JSON.stringify(response.data.Ratings[1].Value, null, 2));
-        console.log("Produced In: " + JSON.stringify(response.data.Country, null, 2));
-        console.log("Language: " + JSON.stringify(response.data.Language, null, 2));
-        console.log(chalk.green("\n============= ") + chalk.magentaBright("S P O T I F Y  T H I S  S O N G ") + chalk.green("============= \n"));
+        console.log(chalk.white.bgGreen("\n============= ") + chalk.magentaBright.bold("M O V I E  T H I S ") + chalk.white.bgGreen("============= \n"));
+        console.log(chalk.bold("Movie Title: ") + JSON.stringify(response.data.Title, null, 2));
+        console.log(chalk.bold("Starring: ") + JSON.stringify(response.data.Actors, null, 2));
+        console.log(chalk.bold("Release Year: ") + JSON.stringify(response.data.Year, null, 2));
+        console.log(chalk.bold("IMDB Rating: ") + JSON.stringify(response.data.Ratings[0].Value, null, 2));
+        console.log(chalk.bold("Rotten Tomatoes Rating: ") + JSON.stringify(response.data.Ratings[1].Value, null, 2));
+        console.log(chalk.bold("Produced In: ") + JSON.stringify(response.data.Country, null, 2));
+        console.log(chalk.bold("Language: ") + JSON.stringify(response.data.Language, null, 2));
+        console.log("------------");
+        console.log(chalk.bold("Plot: ") + JSON.stringify(response.data.Plot, null, 2));
+        console.log(chalk.white.bgGreen("\n============= ") + chalk.magentaBright.bold("M O V I E  T H I S ") + chalk.white.bgGreen("============= \n"));
             })
 
         .catch(function(error) {
@@ -102,7 +106,27 @@ function movieThis() {
             console.log(error.config);
         });
 
-}};
+};
+
+// BANDS IN TOWN FUNCTION
+function concertThis() {
+
+
+        var queryUrl = ("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+
+        axios.get(queryUrl)
+        .then(function(response) {
+            console.log(chalk.black.bgYellow("\n============= ") + chalk.cyanBright.bold("C O N C E R T  T H I S ") + chalk.black.bgYellow("============= \n"));
+            console.log(chalk.bold("Venue: ") + response.data[0].venue.name);
+            console.log(chalk.bold("Location: ") + response.data[0].venue.city + ", " + response.data[0].venue.country);
+            console.log(chalk.bold("Event Date: ") + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            console.log(chalk.black.bgYellow("\n============= ") + chalk.cyanBright.bold("C O N C E R T  T H I S ") + chalk.black.bgYellow("============= \n"));
+            })
+            .catch(function(err) {
+                console.log(err);
+                  });
+        };
+
 
 switch (whatToDo) {
     case "spotify-this-song":
